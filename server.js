5ac11a19830152f69a984127a2f8c9ec2e7c836b
oleast <oleast@stud.ntnu.no>
2017-02-04 18:16:50 +0100

    Fixing linting with Standardjs

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
 
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 // Server/Root File For The Viestinta Project
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 // Include Statments
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 
-var express = require('express')
-var passport = require('passport')
-var bodyparser = require('body-parser')
-var cookieparser = require('cookie-parser')
-var session = require('express-session')
-var morgan = require('morgan')
-var nconf = require('nconf')
+const express = require('express')
+const passport = require('passport')
+const bodyparser = require('body-parser')
+const cookieparser = require('cookie-parser')
+const session = require('express-session')
+// const morgan = require('morgan')
+const nconf = require('nconf')
 
-var PDStrategy = require('passport-openid-connect').Strategy
-var User = require('passport-openid-connect').User
+const PDStrategy = require('passport-openid-connect').Strategy
+// const User = require('passport-openid-connect').User
 
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 // Initial Server Setup
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 
 nconf.argv()
-    .env('__')
-    .file({ file: 'etc/config.json' })
-    .defaults({
-		"http": {
-			"port": 8080,
-			"enforceHTTPS": false
-		},
-		"session": {
-			"secret": "SSSSEEEECCCCRRRREEEETTTTSECRET"
-		},
-		"dataporten": {
-			"enableAuthentication": false
-		}
-    })
+  .env('__')
+  .file({ file: 'etc/config.json' })
+  .defaults({
+    'http': {
+      'port': 8080,
+      'enforceHTTPS': false
+    },
+    'session': {
+      'secret': 'SSSSEEEECCCCRRRREEEETTTTSECRET'
+    },
+    'dataporten': {
+      'enableAuthentication': false
+    }
+  })
 
 const app = express()
 app.set('view options', { pretty: true })
@@ -47,13 +47,12 @@ app.use(cookieparser())
 app.use(bodyparser.urlencoded({extended: true}))
 app.use(bodyparser.json())
 app.use(session({
-	secret: "MagicSealsAndNarwalsDancingTogetherInRainbows",
-	resave: false,
-	saveUninitialized: false
+  secret: 'MagicSealsAndNarwalsDancingTogetherInRainbows',
+  resave: false,
+  saveUninitialized: false
 }))
 
-
-var pd = new PDStrategy(nconf.get("dataporten"))
+var pd = new PDStrategy(nconf.get('dataporten'))
 
 passport.use(pd)
 passport.serializeUser(PDStrategy.serializeUser)
@@ -64,20 +63,20 @@ app.use(passport.session())
 
 let router = express.Router()
 
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 // Main App
-/////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////
 
 router.route('/')
-    .get((req, res) => {
-        res.json({
-            "hello": "world",
-            "user": req.user
-        })
+  .get((req, res) => {
+    res.json({
+      'hello': 'world',
+      'user': req.user
     })
-app.get('/login', passport.authenticate('passport-openid-connect', {"successReturnToOrRedirect": "/"}))
-app.get('/callback', passport.authenticate('passport-openid-connect', {"callback": true, "successReturnToOrRedirect": "/"}))
+  })
+app.get('/login', passport.authenticate('passport-openid-connect', {'successReturnToOrRedirect': '/'}))
+app.get('/callback', passport.authenticate('passport-openid-connect', {'callback': true, 'successReturnToOrRedirect': '/'}))
 
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'))
-});
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'))
+})
