44fc538553a7a39306b0dae3ec1a37ca09a65c72
weedle1912 <weedle1912@gmail.com>
2017-03-09 20:44:33 +0100

    Formated representation of timestamp

--- src/client/components/Message.js
+++ src/client/components/Message.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
+import Schedule from 'material-ui/svg-icons/action/schedule';
 
 const styles = {
 	child: {
@@ -10,13 +12,25 @@ const styles = {
 		margin: 5,
 		textAlign: 'left',
 	},
+  time: {
+    fontSize: '13px',
+    lineHeight: '13px',
+    height: '18px',
+    textAlign: 'left',
+  },
 }
 
 export default class Message extends React.Component {
   render () {
     return (
     	<Paper zDepth={3} style={styles.child}>
-    		<h4>( {this.props.time }) - { this.props.text }</h4>
+    		<h4>{this.props.text}</h4>
+        <FlatButton 
+          style={styles.time}
+          label={<span style={{fontSize: '12px'}}>{this.props.time}</span>}
+          disabled={true}
+          icon={<Schedule style={{width: '18px', height: '18px', margin: '0px'}}/>}
+        />
     	</Paper>
     )
   }
