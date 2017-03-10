d6f7d16c7f8dc501819bd016173083717f10154f
Jacob Tørring <jacob.torring@gmail.com>
2017-03-10 22:56:04 +0100

    Updates database_test with new and better tests

--- src/test/server/database_test.js
+++ src/test/server/database_test.js
@@ -1,7 +1,3 @@
-/**
- * Created by jacob on 20.02.17.
- */
-// Hack for pg issues
 const path = require('path')
 const User = require('../../server/database/models/').User
 const assert = require('assert')
@@ -10,73 +6,74 @@ const db = require('../../server/database/models/index')
 // Extra test frameworks:
 // var should = require('should');
 
+
+/**TEST 1 USER MODEL**/
+
 // Defines alphabet to be used
 var s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
 // Defines the function to generate the names to be used for the test
 // Returns a random String with N length and based on the alphabet s
-
-var testNameGenerator = function (N) {
+var testStringGenerator = function (N) {
   return Array(N).join().split(',').map(function () { return s.charAt(Math.floor(Math.random() * s.length)) }).join('')
 }
 
-// Unique firstName and lastName for each test
-var name1 = testNameGenerator(10)
-var name2 = testNameGenerator(10)
-var name3 = testNameGenerator(10)
-
-
+// Unique firstName and lastName for the test
+var name = testStringGenerator(10)
 
 // Builds the User object with the corresponding name. Does not save the User objects to the database.
-var testUser1 = User.build({name: name1})
-var testUser2 = User.build({name: name2})
-var testUser3 = User.build({name: name3})
+var testUser = User.build({name: name1})
 
-// Creates a tuple array for the test
-var testArray = [ [testUser1, name1],
-                    [testUser2, name2],
-                    [testUser3, name3]
-]
+describe('Test suite 1: User model', function () {
 
-// Test for local user creation
-describe('Test suite: User build', function () {
-  testArray.forEach(function (arrElement, callback) {
-    describe('Build test for name: ' + arrElement[0].name, function () {
-      it('Name to local User object is identical to name generated', function (done) {
-        assert.equal(arrElement[0].name, arrElement[1])
-        done()
-      })
+    /**
+     *  @description Test for database user creation
+     */
+  describe('Database test for name: ' + name, function () {
+    it('Name to User object in database is identical to name generated', function (done) {
+      // Access database and create the user if it doesn't exist
+      db['User']
+        .findOrCreate({
+          where: {name:name},
+          attributes: ['name']
+        })
+
+        // Then compare that user's variables to the variables given
+        .spread(function (user, created) {
+          assert.equal(name, user.name)
+        // Delete the user from the database
+          user.destroy()
+          done()
+        })
     })
   })
 })
 
-// Get the User object from the database
-// Creates a tuple array for the test
-var test2Array = [
-    name1, name2, name3
-]
 
-// Test for database user creation
-describe('Test suite: User create', function () {
-    // For each element in test2Array
-  test2Array.forEach(function (arrElement, callback) {
-    describe('Database test for name: ' + arrElement, function () {
-      it('Name to User object in database is identical to name generated', function (done) {
-                // Access database and create the user if it doesn't exist
-        db['User']
-                    .findOrCreate({
-                      where: {name:arrElement},
-                      attributes: ['name']
-                    })
+/**TEST 2 MESSAGE MODEL**/
 
-                    // Then compare that user's variables to the variables given
-                    .spread(function (user, created) {
-                      assert.equal(arrElement, user.name)
-                    // Delete the user from the database
-                      user.destroy()
-                      done()
-                    })
-      })
+var testString = testStringGenerator(30)
+
+describe('Test suite 2: Message model', function () {
+
+  /**
+   * @description Test for local message
+   */
+  describe('Database test for message: ' + testString, function () {
+    it('Text in Message object in database is identical to testString', function (done) {
+      // Access database and create the message if it doesn't exist
+      db['Message']
+        .findOrCreate({
+          where: {text:text},
+          attributes: ['text']
+        })
+        // Then compare that messages's variables to the variables given
+
+        .spread(function (message, created) {
+          assert.equal(testString, message.text)
+          message.destroy()
+          done()
+        })
     })
   })
 })
