033ff923fed83ee631a8f2d27615cea25d930542
weedle1912 <weedle1912@gmail.com>
2017-03-13 14:12:50 +0100

    Make message ListItem

--- src/client/components/Message.js
+++ src/client/components/Message.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import FlatButton from 'material-ui/FlatButton';
 import ActionSchedule from 'material-ui/svg-icons/action/schedule';
+import {ListItem} from 'material-ui/List';
 
 const styles = {
-	child: {
+	container: {
 		minHeight: 'auto',
 		width: '100%',
 
@@ -22,14 +23,20 @@ const styles = {
 
 export default class Message extends React.Component {
   render () {
+    const timestamp = (
+      <FlatButton 
+        style={styles.time}
+        label={<span style={{fontSize: '12px'}}>{this.props.time}</span>}
+        disabled={true}
+        icon={<ActionSchedule style={{width: '18px', height: '18px', margin: '0px'}}/>}
+      />
+    )
     return (
-    	<Paper zDepth={3} style={styles.child}>
-    		<h4>{this.props.text}</h4>
-        <FlatButton 
-          style={styles.time}
-          label={<span style={{fontSize: '12px'}}>{this.props.time}</span>}
-          disabled={true}
-          icon={<ActionSchedule style={{width: '18px', height: '18px', margin: '0px'}}/>}
+    	<Paper zDepth={3} style={styles.container}>
+    		<ListItem
+          style={styles.listItem}
+          primaryText={this.props.text}
+          secondaryText={timestamp}
         />
     	</Paper>
     )
