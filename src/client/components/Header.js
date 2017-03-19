3fe564e098f4fbc7e042837d12a64a4c6daa8a71
weedle1912 <weedle1912@gmail.com>
2017-03-19 20:10:29 +0100

    Header to be AppBar with logo and menu

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,29 +1,44 @@
 import React from 'react'
 
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
+
 const styles = {
-
-  	parent: {
-	    display: 'flex',
-	    flexDirection: 'column',
-	    alignItems: 'center',
-
-	    maxWidth:   500,
-	    width:      'auto',
-	    height:     'auto',
-  	},
-
 	img: {
-		width: '100%',
+		maxHeight: '40px',
 	},
 };
+
+const logo = (
+	<div>
+		<img src="images/logo_shadow.png" alt="Viestintä Logo" style={styles.img}/>
+	</div>
+)
+
+const rightMenu = (
+	<IconMenu
+		iconButtonElement={
+		<IconButton><NavigationMenu /></IconButton>
+		}
+		targetOrigin={{horizontal: 'right', vertical: 'top'}}
+		anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+	>
+		<MenuItem primaryText="Hjelp" />
+		<MenuItem primaryText="Logg ut" rightIcon={<img src="images/feide_32px.png" style={{width: '24px', height: 'auto'}}/>}/>
+	</IconMenu>
+)
 
 export default class Header extends React.Component {
 	render () {
 	    return (
-	      	<header style={styles.parent}>
-	      		<img src="images/logo_shadow.png" alt="Viestintä Logo" 
-	      		style={styles.img} />
-	      	</header>
+	    	<AppBar 
+	    		style={styles.appBar}
+				iconElementLeft={logo}
+				iconElementRight={rightMenu}
+			/>
 	    )
 	}
 }
