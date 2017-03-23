6fb83495e307dce6e6bdeaa4c4ea33cb455f40aa
weedle1912 <weedle1912@gmail.com>
2017-03-23 09:07:32 +0100

    Feedback menu as side drawer

--- src/client/components/FeedbackBox.js
+++ src/client/components/FeedbackBox.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import socket from '../socket'
+import Drawer from 'material-ui/Drawer'
+import RaisedButton from 'material-ui/RaisedButton'
 
 import FeedbackMenu from './FeedbackMenu'
 import FeedbackWindow from './FeedbackWindow'
+
+const styles = {
+  container: {
+    width: '100%',
+    maxWidth: '500px',
+
+    marginTop: '10px'
+  }
+}
 
 export default class FeedbackBox extends Component {
 
@@ -10,11 +21,13 @@ export default class FeedbackBox extends Component {
     super(props)
     this.state = {
 			// [slow, fast]
-      feedback: [0, 0]
+      feedback: [0, 0],
+      open: false
     }
 
     this.onClick = this.onClick.bind(this)
     this.updateFeedback = this.updateFeedback.bind(this)
+    this.openFeedbackMenu = this.openFeedbackMenu.bind(this)
   }
 
 	// Receiving updated feedback values
@@ -41,15 +54,33 @@ export default class FeedbackBox extends Component {
     this.props.sendFeedback(feedback)
   }
 
+  openFeedbackMenu () {
+    this.setState({open: !this.state.open})
+  }
+
   render () {
     return (
-      <div id='feedback'>
-        <FeedbackMenu onClick={this.onClick} />
-        <FeedbackWindow
-          slow={this.state.feedback[0]}
-          fast={this.state.feedback[1]}
-          updateFeedbackInterval={this.updateFeedbackInterval}
+      <div style={styles.container}>
+        <RaisedButton
+          primary={true}
+          label="Tilbakemelding"
+          fullWidth={true}
+          onTouchTap={this.openFeedbackMenu}
         />
+        <Drawer
+          docked={false}
+          width={300}
+          openSecondary={true}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <FeedbackMenu onClick={this.onClick} />
+          <FeedbackWindow
+            slow={this.state.feedback[0]}
+            fast={this.state.feedback[1]}
+            updateFeedbackInterval={this.updateFeedbackInterval}
+          />
+        </Drawer>
       </div>
     )
   }
