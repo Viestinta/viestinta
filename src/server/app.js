fbcbc878cb9d22b7c2301db3c83dd06df0a32b63
Jacob Tørring <jacob.torring@gmail.com>
2017-03-27 19:03:19 +0200

    Cleans up Redis configuration code

--- src/server/app.js
+++ src/server/app.js
@@ -17,11 +17,8 @@ const redisAdapter = require('socket.io-redis');
 const session = require('express-session')
 
 //'redis' is a relative Docker IP, supply URL in env if it's not Docker
-var redisHost = 'redis'
-if(process.env['REDIS_URL']){
-  redisHost = process.env['REDIS_URL']
-  console.log(redisHost, process.env['REDIS_URL'])
-}
+var redisHost = process.env['REDIS_URL'] || 'redis'
+
 const redis = require("redis").createClient('6379', redisHost)
 const RedisStore = require("connect-redis")(session)
 
