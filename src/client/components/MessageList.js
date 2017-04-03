d8a8eb91dc977be3c99127ae7d73ff1fd02fd48b
weedle1912 <weedle1912@gmail.com>
2017-04-03 23:04:14 +0200

    BottomNavigation to sort messages

--- src/client/components/MessageList.js
+++ src/client/components/MessageList.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import socket from '../socket'
 import Paper from 'material-ui/Paper'
 import {List} from 'material-ui/List'
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
+import ActionSchedule from 'material-ui/svg-icons/action/schedule'
+import ActionThumbsUpDown from 'material-ui/svg-icons/action/thumbs-up-down'
 
 import Message from './Message'
 
 const styles = {
 
-  parent: {
+  container: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -16,14 +19,17 @@ const styles = {
     maxHeight: 400,
     width: '100%',
     height: '100%',
-
+  },
+  parent: {
     marginTop: 10,
     paddingRight: 20,
+
+    width: '100%',
+    height: '100%',
 
     overflowY: 'auto',
     minHeight: 0
   },
-
   child: {
     height: 500,
     width: '100%',
@@ -39,16 +45,14 @@ export default class MessageList extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      messages: [], 
-      sortByVotes: false
+      messages: [],
+      selectedIndex: 0
     }
 
     this.receiveMessage = this.receiveMessage.bind(this)
-    this.getAllMessages = this.getAllMessages.bind(this)
-    this.sortMessageList = this.sortMessageList.bind(this)
-    this.sortListByTime = this.sortListByTime.bind(this)
-    this.sortListByVotes = this.sortListByVotes.bind(this) 
-    this.getClock = this.getClock.bind(this)
+    this.lastTenMessages = this.lastTenMessages.bind(this)
+    this.sortTime = this.sortTime.bind(this)
+    this.sortVotes = this.sortVotes.bind(this)
   }
 
   componentDidMount () {
@@ -74,40 +78,18 @@ export default class MessageList extends Component {
     })
   }
 
-  sortMessageList (list) {
-    console.log("sortMessageList(), sortByVotes: " + this.state.sortByVotes)
-    if (this.state.sortByVotes) {
-      this.sortListByVotes(list)
-    }
-    else {
-      this.sortListByTime(list)
-    }
-  }
-
-  sortListByTime (list) {
-    console.log("Sort list by time")
-    list.sort( (a, b) => { return (( new Date(a.time) ) - ( new Date(b.time)) ) } )
-    
+  sortTime () {
     this.setState({
-      messages: list
+      selectedIndex: 0
     })
+    /* TODO: Sort messages on time */
   }
 
-  sortListByVotes (list) {
-    console.log("Sort list by votes")
-    list.sort( (a, b) => { return ((a.votesUp - a.votesDown) - (b.votesUp - b.votesDown)) } )
-    
+  sortVotes () {
     this.setState({
-      messages: list
+      selectedIndex: 0
     })
-  }
-
-  getClock(time) {
-    if (time.length > 5) {
-        return time.substring(11, 16)
-      }
-
-    return time
+    /* TODO: Sort messages on votes */
   }
 
   render () {
@@ -126,12 +108,32 @@ export default class MessageList extends Component {
       )
     })
 
-    return (
-      <Paper zDepth={3} style={styles.parent}>
-        <List style={styles.child}>
-          {list.reverse()}
-        </List>
+    var sortMenu = (
+      <Paper zDepth={3} style={{width: '100%'}}>
+        <BottomNavigation selectedIndex={this.state.selectedIndex}>
+          <BottomNavigationItem
+            label="Siste"
+            icon={<ActionSchedule />}
+            onTouchTap={this.sortTime}
+          />
+          <BottomNavigationItem
+            label="Stemmer"
+            icon={<ActionThumbsUpDown />}
+            onTouchTap={this.sortVotes}
+          />
+        </BottomNavigation>
       </Paper>
+    )
+
+    return (
+      <div style={styles.container}>
+        <Paper zDepth={3} style={styles.parent}>
+          <List style={styles.child}>
+            {list}
+          </List>
+        </Paper>
+        {this.props.isAdmin ? sortMenu : undefined}
+      </div>
     )
   }
 
