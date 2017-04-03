c2d13c3c96aec8c533dea2a56e5bec22969c6c6a
weedle1912 <weedle1912@gmail.com>
2017-04-03 15:49:02 +0200

    Toggle for isAdmin in AppBar menu

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -5,6 +5,7 @@ import IconButton from 'material-ui/IconButton'
 import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
+import Toggle from 'material-ui/Toggle'
 
 const styles = {
 	img: {
@@ -21,25 +22,28 @@ const logo = (
 	</div>
 )
 
-const rightMenu = (
-	<IconMenu
-		iconButtonElement={
-		<IconButton iconStyle={{fill: '#F8B133'}}><NavigationMenu/></IconButton>
-		}
-		targetOrigin={{horizontal: 'right', vertical: 'top'}}
-		anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-	>
-		<MenuItem primaryText="Hjelp" />
-		<MenuItem primaryText="Logg ut"
-              href="/logout"
-              rightIcon={<img src="images/feide_32px.png"
-                              style={{width: '24px', height: 'auto'}}/>}/>
-
-	</IconMenu>
-)
 
 export default class Header extends React.Component {
 	render () {
+		const rightMenu = (
+			<IconMenu
+				iconButtonElement={
+				<IconButton iconStyle={{fill: '#F8B133'}}><NavigationMenu/></IconButton>
+				}
+				targetOrigin={{horizontal: 'right', vertical: 'top'}}
+				anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+			>
+				<MenuItem>
+					<Toggle label="Foreleser" defaultToggled={this.props.isAdmin} onToggle={this.props.toggleAdmin}/>
+				</MenuItem>
+				<MenuItem primaryText="Hjelp" />
+				<MenuItem primaryText="Logg ut"
+					href="/logout"
+					rightIcon={<img src="images/feide_32px.png"
+									style={{width: '24px', height: 'auto'}}/>}/>
+
+			</IconMenu>
+		)
 	    return (
 	    	<AppBar 
 	    		style={styles.appBar}
