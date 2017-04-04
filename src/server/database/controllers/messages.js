41cc173fe38f24591fe95023fea261d9fcc78df9
Dora Oline Eriksrud <doraoline@gmail.com>
2017-04-04 10:11:11 +0200

    Removed callbacks from vote

--- src/server/database/controllers/messages.js
+++ src/server/database/controllers/messages.js
@@ -119,19 +119,11 @@ module.exports = {
           msg.increment('votesUp', {
             where: 
             { id: req.id }
-          }).then( function() {
-            if (callback) {
-              callback()
-            }
           })
         } else if (req.value === -1) {
           msg.increment('votesDown', {
             where: 
             { id: req.id }
-          }).then( function() {
-            if (callback) {
-              callback()
-            }
           })
         }
       })
