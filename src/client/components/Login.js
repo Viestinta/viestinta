c7eb5278824fdaed4c1238ea0e65608a50fb519a
weedle1912 <weedle1912@gmail.com>
2017-04-07 15:04:55 +0200

    Update of Login component

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -2,6 +2,39 @@ import React, {Component } from 'react'
 import socket from '../socket'
 import RaisedButton from 'material-ui/RaisedButton';
 
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+
+    maxWidth: '600px',
+    width: '100%'
+  },
+  logo: {
+    position: 'fixed',
+    top: '10%',
+
+    maxWidth: '600px',
+    width: '100%',
+    height: 'auto'
+  },
+  button: {
+    position: 'fixed',
+    top: '40%',
+
+    marginTop: '10px'
+  },
+  img: {
+    position: 'fixed',
+    bottom: 0,
+
+    maxWidth: '600px',
+    width: '100%',
+    height: 'auto'
+  }
+}
+
 export default class Login extends Component {
   
   constructor (props) {
@@ -20,15 +53,17 @@ export default class Login extends Component {
 
   render () {
     return (
-      <div>
+      <div style={styles.container}>
+        <img src="images/logo_shadow.png" alt="Viestintä Logo" style={styles.logo}/>
         <RaisedButton
-            style={{marginTop: '10px'}}
+            style={styles.button}
             backgroundColor="#be1527"
             labelColor="#ffffff"
             href="/login"
             label="Logg inn"
             icon={<img src="images/feide_100px_white.png" style={{width: 20, height: 'auto'}}/>}
         />
+        <img src="images/technologies.png" style={styles.img}/>
       </div>
     )
   }
