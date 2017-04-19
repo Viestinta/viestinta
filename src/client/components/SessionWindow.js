620b5d9460462c417084e299a8f6184f0a5d97e9
weedle1912 <weedle1912@gmail.com>
2017-04-20 00:51:40 +0200

    Added m-ui components and example of layout

--- src/client/components/SessionWindow.js
+++ src/client/components/SessionWindow.js
@@ -10,7 +10,11 @@ import RaisedButton from 'material-ui/RaisedButton'
 import LectureTable from './LectureTable'
 import LectureWrapper from './LectureWrapper'
 
-import DataTables from 'material-ui-datatables';    
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import {Step, Stepper, StepLabel} from 'material-ui/Stepper'
+import DatePicker from 'material-ui/DatePicker'
+import TimePicker from 'material-ui/TimePicker'  
 
 const styles = {
 
@@ -45,12 +49,27 @@ export default class SessionWindow extends Component {
 
         this.state = {
             selectedLecture: undefined,
+
+            openDialog: false,
+            stepIndex: 0,
+
+            courseCode: undefined,
+            name: undefined,
+            description: undefined,
+            startTime: undefined,
+            endTime: undefined
         }
 
         this.setSelectedLecture = this.setSelectedLecture.bind(this)
         this.connectToLecture = this.connectToLecture.bind(this)
-        this.createNewLecture = this.createNewLecture.bind(this)
         this.disconnectFromLecture = this.disconnectFromLecture.bind(this)
+        /* New lecture */
+        this.createNewLecture = this.createNewLecture.bind(this)
+        this.handleOpen = this.handleOpen.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+        this.handleNext = this.handleNext.bind(this)
+        this.handlePrev = this.handlePrev.bind(this)
+        this.getStepContent = this.getStepContent.bind(this)
     }
 
     setSelectedLecture (lecture) {
@@ -75,37 +94,135 @@ export default class SessionWindow extends Component {
     }
 
     createNewLecture (courseCode, lectureName) {
-        //TODO: Håndteres av Socket?
-        axios.post('/create-lecture', {courseCode: courseCode, lectureName: lectureName}).then(function () {
-            console.log('[SessionWindow][Axios] Create new lecture, course: ' + courseCode + ' name: ' + lectureName)
+        const lectureInfo = {
+            courseCode: this.state.courseCode, 
+            name: this.state.name, 
+            /*optional*/
+            description: this.state.description,
+            startTime: this.state.startTime,
+            endTime: this.state.endTime,
+            isActive: true
+        }
+        console.log('[SessionWindow] createNewLecture(), lectureInfo:')
+        console.log('- courseCode:  ' + lectureInfo.courseCode)
+        console.log('- name:        ' + lectureInfo.name)
+        console.log('- description: ' + lectureInfo.description)
+        console.log('- startTime:   ' + lectureInfo.startTime)
+        console.log('- endTime:     ' + lectureInfo.endTime)
+        //socket.emit('create-lecture', lectureInfo)
+        this.handleClose()
+    }
+
+    handleOpen () {
+        this.setState({openDialog: true})
+    }
+
+    handleClose () {
+        this.setState({
+            openDialog: false,
+            stepIndex: 0,
+
+            courseCode: undefined,
+            name: undefined,
+            description: undefined,
+            startTime: undefined,
+            endTime: undefined
         })
+    }
+
+    handleNext () {
+        const stepIndex = this.state.stepIndex
+        this.setState({
+            stepIndex: stepIndex + 1,
+        })
+    }
+
+    handlePrev () {
+        const stepIndex = this.state.stepIndex
+        if (stepIndex > 0) {
+            this.setState({stepIndex: stepIndex - 1});
+        }
+    }
+
+    getStepContent () {
+        switch (this.state.stepIndex) {
+        case 0:
+            return (<DatePicker hintText="Portrait Dialog" />)
+        case 1:
+            return (<TimePicker format="24hr" hintText="24hr Format"/>)
+        case 2:
+            return 'Dette er det du har valgt.'
+        default:
+            return ''
+        }
     }
 
     render () {
         return (
             <div style={styles.container}>
-            <Header userName={this.props.user.name} isAdmin={this.props.isAdmin} toggleAdmin={this.props.toggleAdmin}/>
-            <Paper zDepth={3} style={styles.session}>
-                <Subheader style={{width: 'auto'}}>
-                    {this.state.selectedLecture ? this.state.selectedLecture.course.code + ' : ' + this.state.selectedLecture.course.name : 'Velg forelesning fra listen under'}
-                </Subheader>
-                
-                { (this.state.selectedLecture || this.props.isAdmin) ?
-                    <RaisedButton
-                        primary={true}
-                        label= { this.state.selectedLecture ? "Forlat forelesning" : "Lag ny" }
-                        onTouchTap={ this.state.selectedLecture ? this.disconnectFromLecture : this.createNewLecture }
-                    /> : undefined
+                {/* App header */}
+                <Header userName={this.props.user.name} isAdmin={this.props.isAdmin} toggleAdmin={this.props.toggleAdmin}/>
+                {/* Session header */}
+                <Paper zDepth={3} style={styles.session}>
+                    <Subheader style={{width: 'auto'}}>
+                        {this.state.selectedLecture ? this.state.selectedLecture.course.code + ' : ' + this.state.selectedLecture.course.name : 'Velg forelesning fra listen under'}
+                    </Subheader>
+                    
+                    { (this.state.selectedLecture || this.props.isAdmin) ?
+                        <RaisedButton
+                            primary={true}
+                            label= { this.state.selectedLecture ? "Forlat forelesning" : "Lag ny" }
+                            onTouchTap={ this.state.selectedLecture ? this.disconnectFromLecture : this.handleOpen }
+                        /> : undefined
+                    }
+                </Paper>
+                {/* Dialog on 'create lecture' */}
+                <Dialog
+                    title="Lag ny forelesning"
+                    actions={<FlatButton label="Avbryt" primary={true} onTouchTap={this.handleClose}/>}
+                    modal={false}
+                    open={this.state.openDialog}
+                    onRequestClose={this.handleClose}
+                >
+                    <Stepper activeStep={this.state.stepIndex}>
+                        <Step>
+                            <StepLabel>Påkrevd info</StepLabel>
+                        </Step>
+                        <Step>
+                            <StepLabel>Valgfri info</StepLabel>
+                        </Step>
+                        <Step>
+                            <StepLabel>Fullfør</StepLabel>
+                        </Step>
+                    </Stepper>
+                    <div style={{margin: '0 16px'}}>
+                        <div>
+                            {this.getStepContent()}
+                            <div style={{marginTop: 12}}>
+                                <FlatButton
+                                label="Tilbake"
+                                disabled={this.state.stepIndex === 0}
+                                onTouchTap={this.handlePrev}
+                                style={{marginRight: 12}}
+                                />
+                                <RaisedButton
+                                label={this.state.stepIndex === 2 ? 'Fullfør' : 'Neste'}
+                                primary={true}
+                                onTouchTap={this.state.stepIndex === 2 ? this.createNewLecture : this.handleNext}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </Dialog>
+                {/* Display session or table of sessions */}
+                { this.state.selectedLecture ? 
+                    <LectureWrapper
+                        isAdmin={this.props.isAdmin} 
+                        lecture={this.state.selectedLecture}
+                    /> 
+                    : 
+                    <LectureTable setSelectedLecture={this.setSelectedLecture}/> 
                 }
-            </Paper>
-            { this.state.selectedLecture ? 
-                <LectureWrapper
-                    isAdmin={this.props.isAdmin} 
-                    lecture={this.state.selectedLecture}
-                /> 
-                : 
-                <LectureTable setSelectedLecture={this.setSelectedLecture}/> 
-            }
             </div>
         )
     }
